9a0b92a36a02b0c4ec3344b2688670f21b01e1f8
romansukhatskyi <romansukhatskyi@gmail.com>
2017-02-24 13:28:45 +0200

    Modified file Unlock package

--- Unlock package.sql
+++ Unlock package.sql
@@ -3,3 +3,17 @@ UPDATE [SysPackage] SET [IsChanged] = 1, [IsLocked] = 1, [InstallType] = 0 WHERE
 UPDATE [SysSchema] SET [IsChanged] = 1, [IsLocked] = 1 WHERE [syspackageid] = (SELECT [id] FROM [syspackage] WHERE [name] = 'WebitelSMS')
 
 UPDATE [syspackageschemadata] SET [IsChanged] = 1, [IsLocked] = 1 WHERE [syspackageid] = (SELECT [id] FROM [syspackage] WHERE [name] = 'WebitelSMS')
+
+
+--Для разблокировки пакета необходимо изменить первый скрипт на:
+UPDATE [SysPackage]
+SET
+[IsChanged] = 1,
+[IsLocked] = 1,
+[InstallType] = 0,
+[Maintainer] = (select [TextValue]
+from [SysSettingsValue]
+join [SysSettings]
+on [SysSettingsValue].[SysSettingsId] = [SysSettings].[Id]
+WHERE [Code] = 'Maintainer')
+WHERE [Name] = 'WebitelCore'
